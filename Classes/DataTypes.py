e13e87a6c89a8a10cee2a82f71381a6a87941340
hotdrinkbrian <hotdrink1104@gmial.com>
2019-04-23 00:34:22 +0200

    save

--- Classes/DataTypes.py
+++ Classes/DataTypes.py
@@ -60,6 +60,20 @@ class pkl_df:
         self.df      = (self.df)[dropone]
         self.events  = len(self.df)
         self.train_val_test_ratio = {'train':0.6,'val':0.2,'test':0.2}
+ 
+     
+
+        #self.jets    = (self.df).groupby('jetIndex_0') 
+
+        #self.jets_no_match = (self.df).groupby('jetIndex_0')  
+        #self.jets    = (self.df[self.df['isGenMatched']==1]).groupby('jetIndex_0')
+
+
+    #def gen_matching(self):    self.df = self.df[ self.df['isGenMatched']==1 ]
+    def gen_matching(self, gm):
+        if gm:    self.jets    = (self.df[self.df['isGenMatched']==1]).groupby('jetIndex_0')
+        else :    self.jets    = (self.df).groupby('jetIndex_0')  
+
 
     def set_xs(self,xs):
         self.xs       = xs
@@ -128,22 +142,217 @@ class pkl_df:
         return k_fold_sgn, k_fold_bkg
 
 
+    def splitTV_0(self,n_fold):
+        df_bkg_dic_j0     = {}
+        df_bkg_dic_j1     = {}
+        k_fold_bkg_dic_j0 = {i:[] for i in range(n_fold)}
+        k_fold_bkg_dic_j1 = {i:[] for i in range(n_fold)}
+        k_fold_bkg_j0     = []
+        k_fold_bkg_j1     = []
+
+        df         = self.df
+        df_bkg     = df[df['is_signal_new']==0]
+        df_bkg_j0  = df_bkg[df_bkg['jetIndex_0']==0]
+        df_bkg_j1  = df_bkg[df_bkg['jetIndex_0']==1]
+        df_sgn     = df[df['is_signal_new']==1]
+
+        qcd_n_dic_j0 = dict(df_bkg_j0.groupby('xs').size() )
+        qcd_n_dic_j1 = dict(df_bkg_j1.groupby('xs').size() )  
+
+        print '>>>> j0:'
+        for key, item in qcd_n_dic_j0.iteritems():
+            print '#events for xs=', key, ': ', item
+            tmp_df = df_bkg_j0[df_bkg_j0['xs']==key]
+          
+            df_bkg_dic_j0[key] = np.array_split(tmp_df, n_fold)
+            for i in range(n_fold):    k_fold_bkg_dic_j0[i].append( df_bkg_dic_j0[key][i] )
+
+        print '>>>> j1:'
+        for key, item in qcd_n_dic_j1.iteritems():
+            print '#events for xs=', key, ': ', item
+            tmp_df = df_bkg_j1[df_bkg_j1['xs']==key]
+           
+            df_bkg_dic_j1[key] = np.array_split(tmp_df, n_fold)
+            for i in range(n_fold):    k_fold_bkg_dic_j1[i].append( df_bkg_dic_j1[key][i] )
+
+
+
+        k_fold_sgn          = np.array_split(df_sgn, n_fold)
+        for i in range(n_fold):    k_fold_bkg_j0.append( pd.concat(k_fold_bkg_dic_j0[i]) )
+        for i in range(n_fold):    k_fold_bkg_j1.append( pd.concat(k_fold_bkg_dic_j1[i]) )
+
+        return k_fold_sgn, k_fold_bkg_j0, k_fold_bkg_j1
+
+
+
+    def splitTV_1(self, n_fold, upLmt=100000):
+        #upLmt             = 100000 
+        n_jets            = 2  
+        df_bkg_dic        = {}
+        k_fold_bkg_dic    = {}
+        k_fold_bkg        = {} 
+        for j in range(n_jets): 
+            df_bkg_dic[j]     = {}
+            k_fold_bkg_dic[j] = {i:[] for i in range(n_fold)}
+            k_fold_bkg[j]     = []
+
+        df         = self.df
+        df_sgn     = df[df['is_signal_new']==1]
+        df_bkg_t   = df[df['is_signal_new']==0]
+        df_bkg     = {}
+        qcd_n_dic  = {}
+        for j in range(n_jets):
+            df_bkg[j]  = df_bkg_t[df_bkg_t['jetIndex_0']==j] 
+            qcd_n_dic[j] = dict(df_bkg[j].groupby('xs').size() )
+
+        for j in range(n_jets):
+            print '>>>> j'+str(j)+':'
+            for key, item in qcd_n_dic[j].iteritems():
+                print '#events for xs=', key, ': ', item
+                #tmp_df             = df_bkg[j][df_bkg[j]['xs']==key]
+                tmp_df             = (df_bkg[j][df_bkg[j]['xs']==key])[:upLmt]
+                print '#events for xs=', key, '(with upper limit): ', len(tmp_df)   
+
+                df_bkg_dic[j][key] = np.array_split(tmp_df, n_fold)
+                for i in range(n_fold):    k_fold_bkg_dic[j][i].append( df_bkg_dic[j][key][i] )
+
+        k_fold_sgn = np.array_split(df_sgn, n_fold)
+
+        for j in range(n_jets):
+            for i in range(n_fold):  
+                k_fold_bkg[j].append( pd.concat(k_fold_bkg_dic[j][i]) )
+
+        return k_fold_sgn, k_fold_bkg[0], k_fold_bkg[1]
 
 
 
 
 
 
+    def splitTV(self, n_fold=6, upLmt=None, n_jets=4):
+        out_dic           = {}
+        df_bkg_dic        = {}
+        #k_fold_bkg_dic    = {}
+        #k_fold_bkg        = {} 
+        k_fold_bkg        = []
+        k_fold_bkg_dic    = {i:[] for i in range(n_fold)}
+        #for j in range(n_jets): 
+        #    df_bkg_dic[j]     = {}
+        #    k_fold_bkg_dic[j] = {i:[] for i in range(n_fold)}
+        #    k_fold_bkg[j]     = []
+
+        df         = self.df
+        df_sgn     = df[df['is_signal_new']==1]
+        df_bkg_t   = df[df['is_signal_new']==0]
+        #df_bkg     = {}
+        #qcd_n_dic  = {}
+        #for j in range(n_jets):
+        #    df_bkg[j]  = df_bkg_t[df_bkg_t['jetIndex_0']==j] 
+        #    qcd_n_dic[j] = dict(df_bkg[j].groupby('xs').size() ) 
+        qcd_n_dic  = dict( df_bkg_t.groupby('xs').size() )
+        """
+        for j in range(n_jets):
+            print '>>>> j'+str(j)+':'
+            for key, item in qcd_n_dic[j].iteritems():
+                print '#events for xs=', key, ': ', item
+                #tmp_df             = df_bkg[j][df_bkg[j]['xs']==key]
+                tmp_df             = (df_bkg[j][df_bkg[j]['xs']==key])[:upLmt]
+                print '#events for xs=', key, '(with upper limit): ', len(tmp_df)   
+
+                df_bkg_dic[j][key] = np.array_split(tmp_df, n_fold)
+                for i in range(n_fold):    k_fold_bkg_dic[j][i].append( df_bkg_dic[j][key][i] )
+        """
+        for key, item in qcd_n_dic.iteritems():
+            print '#events for xs=', key, ': ', item
+            tmp_df             = (df_bkg_t[df_bkg_t['xs']==key])[:upLmt]
+            print '#events for xs=', key, '(with upper limit): ', len(tmp_df)   
+
+            df_bkg_dic[key]    = np.array_split(tmp_df, n_fold)
+            for i in range(n_fold):    k_fold_bkg_dic[i].append( df_bkg_dic[key][i] )
+
+
+ 
+        k_fold_sgn = np.array_split(df_sgn, n_fold)
+        for i in range(n_fold):    k_fold_bkg.append( pd.concat(k_fold_bkg_dic[i]) )
+        #for j in range(n_jets):
+        #    for i in range(n_fold):  
+        #        k_fold_bkg[j].append( pd.concat(k_fold_bkg_dic[j][i]) )
+
+        out_dic['sgn'] = k_fold_sgn 
+        out_dic['bkg'] = k_fold_bkg
+        return out_dic 
+
+
+
+    def splitTV_4(self, n_fold, upLmt=100000):
+        n_jets            = 4
+        df_sgn_dic        = {}
+        df_bkg_dic        = {}
+        k_fold_bkg_dic    = {}
+        k_fold_bkg        = {}
+
+        k_fold_sgn_dic    = {} 
+        k_fold_sgn        = {}
+        for j in range(n_jets):
+            df_bkg_dic[j]     = {}
+            k_fold_bkg_dic[j] = {i:[] for i in range(n_fold)}
+            k_fold_bkg[j]     = []
+
+            k_fold_sgn_dic[j] = {i:[] for i in range(n_fold)}
+            k_fold_sgn[j]     = []
+
+        df         = self.df
+        #df_sgn     = df[df['is_signal_new']==1]
+        df_sgn_t   = df[df['is_signal_new']==1]
+        df_bkg_t   = df[df['is_signal_new']==0]
+        df_sgn     = {} 
+        df_bkg     = {}
+        qcd_n_dic  = {}
+        for j in range(n_jets):
+            df_bkg[j]  = df_bkg_t[df_bkg_t['jetIndex_0']==j]
+            qcd_n_dic[j] = dict(df_bkg[j].groupby('xs').size() )
+
+            df_sgn[j]  = df_sgn_t[df_bkg_t['jetIndex_0']==j]
+
+
+        for j in range(n_jets):
+            print '>>>> j'+str(j)+':'
+            for key, item in qcd_n_dic[j].iteritems():
+                print '#events for xs=', key, ': ', item
+                #tmp_df             = df_bkg[j][df_bkg[j]['xs']==key]
+                tmp_df             = (df_bkg[j][df_bkg[j]['xs']==key])[:upLmt]
+                print '#events for xs=', key, '(with upper limit): ', len(tmp_df)
+
+                df_bkg_dic[j][key] = np.array_split(tmp_df, n_fold)  
+                for i in range(n_fold):    k_fold_bkg_dic[j][i].append( df_bkg_dic[j][key][i] )
+         
+            df_sgn_dic[j] = np.array_split(df_sgn[j], n_fold) 
+            for i in range(n_fold):    k_fold_sgn_dic[j][i].append( df_sgn_dic[j][i] ) 
+
+        #k_fold_sgn = np.array_split(df_sgn, n_fold)
+
+        for j in range(n_jets):
+            for i in range(n_fold):
+                k_fold_bkg[j].append( pd.concat(k_fold_bkg_dic[j][i]) )
+                k_fold_sgn[j].append( pd.concat(k_fold_sgn_dic[j][i]) )
+ 
+        out_dic = {}
+        out_dic['sgn'] = k_fold_sgn 
+        out_dic['bkg'] = k_fold_bkg
+        return out_dic
+        #return k_fold_sgn, k_fold_bkg[0], k_fold_bkg[1], k_fold_bkg[2], k_fold_bkg[3]
 
 
 
 
 
-
-
-
-
-
+    def splitTV_sgn(self, n_fold=3, upLmt=None, n_jets=4):
+        out_dic        = {}
+        df             = self.df
+        df_sgn         = df[df['is_signal_new']==1]
+        k_fold_sgn     = np.array_split(df_sgn, n_fold)
+        out_dic['sgn'] = k_fold_sgn
+        return out_dic 
 
 
 
